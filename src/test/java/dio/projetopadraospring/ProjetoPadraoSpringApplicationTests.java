a5a3f662e91e45685be46ce60ed05902b89accc7
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-10-17 08:51:10 -0300

    Criando Projeto

---
+++ src/test/java/dio/projetopadraospring/ProjetoPadraoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.projetopadraospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPadraoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
