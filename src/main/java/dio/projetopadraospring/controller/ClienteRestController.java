2ceb05edc0341571831f94c196a4215d0e68c128
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-10-17 09:34:56 -0300

    Feat - Controller

---
+++ src/main/java/dio/projetopadraospring/controller/ClienteRestController.java
@@ -0,0 +1,44 @@
+package dio.projetopadraospring.controller;
+
+
+import dio.projetopadraospring.model.Cliente;
+import dio.projetopadraospring.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteRestController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Integer id) throws ChangeSetPersister.NotFoundException {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+}
